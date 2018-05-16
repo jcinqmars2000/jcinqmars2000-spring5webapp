df480741404a8d2692bfa96d8f3742eca813a5ab
jcinq <jcinq@Earth.fios-router.home>
2018-05-16 17:49:42 -0400

    Initializing Data with Spring Framework Events

---
+++ src/main/java/com/responsiblenrg/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,54 @@
+package com.responsiblenrg.spring5webapp.bootstrap;
+
+
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import com.responsiblenrg.spring5webapp.model.Author;
+import com.responsiblenrg.spring5webapp.model.Book;
+import com.responsiblenrg.spring5webapp.repositories.AuthorRepository;
+import com.responsiblenrg.spring5webapp.repositories.BookRepository;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
